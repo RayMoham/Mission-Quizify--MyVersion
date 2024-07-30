9aa33d3bd77c0bb5d0108f07df05bcb1c545c2b7
Rayan Kaleem Mohammed <125421584+RayMoham@users.noreply.github.com>
2024-07-30 10:46:45 -0400

    Update task_4.py

--- tasks/task_4/task_4.py
+++ tasks/task_4/task_4.py
@@ -33,9 +33,9 @@ class EmbeddingClient:
         # Initialize the VertexAIEmbeddings client with the given parameters
         # Read about the VertexAIEmbeddings wrapper from Langchain here
         # https://python.langchain.com/docs/integrations/text_embedding/google_generative_ai
-        self.client = VertexAIEmbeddings(
+        self.client = VertexAIEmbeddings(model_name=model_name, project=project, location=location)
             #### YOUR CODE HERE ####
-        )
+        
         
     def embed_query(self, query):
         """
@@ -62,7 +62,7 @@ class EmbeddingClient:
 
 if __name__ == "__main__":
     model_name = "textembedding-gecko@003"
-    project = "YOUR PROJECT ID HERE"
+    project = "axiomatic-area-423218-h7"
     location = "us-central1"
 
     embedding_client = EmbeddingClient(model_name, project, location)
